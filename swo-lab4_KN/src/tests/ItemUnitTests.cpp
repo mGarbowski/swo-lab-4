4b4e3d349d355c7af6e57e3a257ba0f38b308c93
mgarbowski <mikolaj.garbowski@gmail.com>
2024-05-28 14:08:43 +0200

    refactor Item unit tests to do assertions using getters

--- swo-lab4_KN/src/tests/ItemUnitTests.cpp
+++ swo-lab4_KN/src/tests/ItemUnitTests.cpp
@@ -4,50 +4,59 @@
 
 #define CATCH_CONFIG_MAIN
 
-TEST_CASE("Item toString method", "[toString]") {
+TEST_CASE("Item toString method", "[Item]") {
     Item item("Test Item", 10, 20);
-    REQUIRE(item.toString() == "Test Item, 10, 20");
+    REQUIRE(item.getQuality() == 20);
+    REQUIRE(item.getDaysRemaining() == 10);
 }
 
-TEST_CASE("Item updateQuality method", "[updateQuality]") {
+TEST_CASE("Item updateQuality method", "[Item]") {
 
     SECTION("Quality decreases by 1 when daysRemaining is positive") {
         Item item("Test Item", 10, 20);
         item.updateQuality();
-        REQUIRE(item.toString() == "Test Item, 9, 19");
+        REQUIRE(item.getQuality() == 19);
+        REQUIRE(item.getDaysRemaining() == 9);
     }
 
     SECTION("Quality decreases by 2 when daysRemaining is zero or less") {
         Item item("Test Item", 0, 20);
         item.updateQuality();
-        REQUIRE(item.toString() == "Test Item, -1, 18");
+        REQUIRE(item.getQuality() == 18);
+        REQUIRE(item.getDaysRemaining() == -1);
 
         Item item2("Test Item", -1, 20);
         item2.updateQuality();
-        REQUIRE(item2.toString() == "Test Item, -2, 18");
+        REQUIRE(item2.getQuality() == 18);
+        REQUIRE(item2.getDaysRemaining() == -2);
     }
 
     SECTION("Quality does not go below MIN_VALUE") {
         Item item("Test Item", 0, 1);
         item.updateQuality();
-        REQUIRE(item.toString() == "Test Item, -1, 0");
+        REQUIRE(item.getQuality() == 0);
+        REQUIRE(item.getDaysRemaining() == -1);
 
         Item item2("Test Item", 10, 0);
         item2.updateQuality();
-        REQUIRE(item2.toString() == "Test Item, 9, 0");
+        REQUIRE(item2.getQuality() == 0);
+        REQUIRE(item2.getDaysRemaining() == 9);
 
         Item item3("Test Item", -1, 0);
         item3.updateQuality();
-        REQUIRE(item3.toString() == "Test Item, -2, 0");
+        REQUIRE(item3.getQuality() == 0);
+        REQUIRE(item3.getDaysRemaining() == -2);
     }
 
     SECTION("Days remaining decreases by 1 each update") {
         Item item("Test Item", 10, 20);
         item.updateQuality();
-        REQUIRE(item.toString() == "Test Item, 9, 19");
+        REQUIRE(item.getQuality() == 19);
+        REQUIRE(item.getDaysRemaining() == 9);
 
         item.updateQuality();
-        REQUIRE(item.toString() == "Test Item, 8, 18");
+        REQUIRE(item.getQuality() == 18);
+        REQUIRE(item.getDaysRemaining() == 8);
     }
 }
 
