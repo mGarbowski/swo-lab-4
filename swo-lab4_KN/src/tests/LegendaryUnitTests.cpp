ba3f612339e68085ff924a64f656643d634340ca
mgarbowski <mikolaj.garbowski@gmail.com>
2024-05-28 14:09:44 +0200

    refactor Legendary unit tests to do assertions using getters

--- swo-lab4_KN/src/tests/LegendaryUnitTests.cpp
+++ swo-lab4_KN/src/tests/LegendaryUnitTests.cpp
@@ -1,11 +1,12 @@
 #include "items/Legendary.h"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Legendary updateQuality method", "[updateQuality]") {
+TEST_CASE("Legendary updateQuality method", "[Legendary]") {
 
     SECTION("Quality and days remaining do not change") {
         Legendary item("Test Item", 10, 20);
         item.updateQuality();
-        REQUIRE(item.toString() == "Test Item, 10, 20");
+        REQUIRE(item.getQuality() == 20);
+        REQUIRE(item.getDaysRemaining() == 10);
     }
 }
